2742543c2a291ec3b2d39536d278687879e08a86
anas sawalha <anas.swalha@yahoo.com>
2023-03-13 12:01:04 +0300

    gg

--- py_serve.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-cd ~/Sites/quartz/public; python3 -m http.server
--- scripts/compile.bat
+++ scripts/compile.bat
@@ -1,7 +1,7 @@
- 
+@echo off
 
 REM Define variables for directories used in the script
-set obsidianDir=c:\Users\User\Desktop\Obsidian
+set obsidianDir=c:\Users\User\Desktop\ObsidianB
 set quartzDir=%obsidianDir%\quartz
 set contentDir=%quartzDir%\content
 set publicDir=%quartzDir%\public
@@ -13,16 +13,47 @@ echo Running script...
 
 cd %hugoObsidianDir% && (
     echo [Success] Chnaged directory successfully.
-) 
-del /S /Q %contentDir%\* 
-del /S /Q %publicDir%\*
+) || (
+    echo Error in changing directory.
+    echo [Failed] Failed
+    
+)
+del /S /Q %contentDir%\* >nul 2>&1 && (
+    echo [Success] Content files deleted successfully.
+) || (
+    echo [Failed] Deletion failed for quartz/content files.
+    echo [Failed] Failed
+    echo [Failed] Error message: %errorlevel%
+    
+)
+del /S /Q %publicDir%\* >nul 2>&1 && (
+    echo [Success] Public files deleted successfully.
+) || (
+    echo [Failed] Deletion failed for quartz/public files.
+    echo [Failed] Failed
+    echo [Failed] Error message: %errorlevel%
+   
+)
 
+%exportDir%\obsidian-export --add-titles --frontmatter=always %obsidianDir%\%obsidianVault% %contentDir%  >nul 2>&1 || (
+    echo [x] Error in running Obsidian Export.
+    echo [x] Failed
+    for /f "tokens=* delims=" %%i in ('%exportDir%\obsidian-export --add-titles --frontmatter=always %obsidianDir%\%obsidianVault% %contentDir%  2^>^&1') do (
+        set errorMessage=%%i
+    )
+    echo [x] Error message: %errorMessage%
+    goto :end
+)
 
-echo  obsidian-export
-%exportDir%\obsidian-export --add-titles --frontmatter=always %obsidianDir%\%obsidianVault% %contentDir% 
-
-go run %hugoObsidianDir% -input=%contentDir% -output=%quartzDir%\assets\indices -index -root=%quartzDir% --baseUrl=localhost:1313
-@REM go run c:\Users\User\Desktop\Obsidian\hugo-obsidian -input=c:\Users\User\Desktop\Obsidian\quartz\content -output=c:\Users\User\Desktop\Obsidian\quartz\assets\indices -index -root=c:\Users\User\Desktop\Obsidian\quartz 
+go run %hugoObsidianDir% -input=%contentDir% -output=%quartzDir%\assets\indices -index -root=%quartzDir% >nul 2>&1 || (
+    echo [x] Hugo build failed.
+    echo [x] Failed
+    for /f "tokens=* delims=" %%i in ('go run %hugoObsidianDir% -input=%contentDir% -output=%quartzDir%\assets\indices -index -root=%quartzDir% 2^>^&1') do (
+        set errorMessage=%%i
+    )
+    echo [x] Error message: %errorMessage%
+    goto :end
+)
 
 cd %quartzDir% && hugo --minify && (
     echo [Success] hugo --minify ran successfully.
@@ -34,5 +65,4 @@ cd %quartzDir% && hugo --minify && (
 REM If the script reaches here, it means that everything completed successfully
 echo [Success] Script completed successfully.
 
-
-
+:end
