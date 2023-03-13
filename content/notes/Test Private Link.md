e819448a75c82353bdea9612732996b57938fe37
anas sawalha <anas.swalha@yahoo.com>
2023-03-14 01:35:41 +0300

    ad

--- content/notes/Test Private Link.md
+++
@@ -1,6 +0,0 @@
----
-title: Test Private Link
----
-
-ddd
-d
--- run.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-set watchScript= scripts\watch.bat
-set serveScript= scripts\serve.bat
-
-start cmd /k "%watchScript%"
-start cmd /k "%serveScript%"
